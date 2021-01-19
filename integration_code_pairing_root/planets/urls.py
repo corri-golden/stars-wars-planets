a0e7e2a3aebb218771a6c45079b786c87b7d0dc2
Corri Golden <corri.golden@gmail.com>
2021-01-19 12:28:41 -0600

    add search by planet population endpoint

--- integration_code_pairing_root/planets/urls.py
+++ integration_code_pairing_root/planets/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-# from . import views
 from .views import *
 
 urlpatterns = [
     path('<int:id>/', get_by_id),
-    # path('^', views.get_planet_by_pop)
     path('population/<int:population>/', get_planet_by_pop)
 ]
